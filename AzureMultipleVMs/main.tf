2917dfb92f72244f6b217e99c7b4efdcabd28e66
PrasannaCloudPro <55081476+PrasannaCloudSME@users.noreply.github.com>
2021-08-24 10:00:46 +0530

    Update main.tf

--- AzureMultipleVMs/main.tf
+++ AzureMultipleVMs/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-   subscription_id = "95b4f162-1d36-4805-9214-18ade2c3a528"
-   client_id       = "45261193-fba5-47eb-a59a-67141287ddfc"
-   client_secret   = "HRulbmErT1l0q0MroR5hUOTiFGNLPgl-v2"
-   tenant_id       = "1f49e8e6-d76f-4f6b-8ab5-fb24b5e2c00a"
+   subscription_id = "*******************************"
+   client_id       = "*******************************"
+   client_secret   = "*******************************"
+   tenant_id       = "*******************************"
  }
 
 resource "azurerm_resource_group" "rg" {
